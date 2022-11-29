fc0ab39f5eda41664de9c49ce1691b5d213ae1d8
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 20:45:14 +0530

    Break and Continue statement in java programming

---
+++ Java demos/src/com/capgimini/loop/BreakandContinue.java
@@ -0,0 +1,18 @@
+package com.capgimini.loop;
+
+public class BreakandContinue {
+
+		public static void main(String[] args) 
+		{	
+	         for(int i=1; i<=10; i++)
+	         {
+	        	 if(i == 1)
+	        	 {
+	        		 continue;   // break;
+	        	 }
+	        	 System.out.println(i);
+	         }
+		}
+	}
+
+
