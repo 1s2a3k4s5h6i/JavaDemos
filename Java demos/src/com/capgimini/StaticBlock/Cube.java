4329e2b2b91d6e5f767de534793c612598b1e564
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 21:00:14 +0530

    Static blocks in java programming

---
+++ Java demos/src/com/capgimini/StaticBlock/Cube.java
@@ -0,0 +1,21 @@
+package com.capgimini.StaticBlock;
+
+public class Cube {
+	
+		static
+		{
+		System.out.println("static block is invoked");
+		}
+		static int calculateCube(int side)
+		{
+		return (side*side*side);
+		}
+
+		public static void main(String[] args) {
+		
+			//System.out.println("Cube value of 5 is: "+calculateCube(5));
+			
+		System.out.println("Cube value of 5 is: "+Cube.calculateCube(5));
+		}
+		}
+
