23b4609ec40df94fa8ef07cadd5f17289abb1508
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 19:51:16 +0530

    TypeCasting Program

---
+++ Java demos/src/com/capgemini/primitive/TypeCasting.java
@@ -0,0 +1,15 @@
+package com.capgemini.primitive;
+
+public class TypeCasting 
+	 {
+
+		public static void main(String[] args) {
+			
+	       float f = 10.532f;
+	       long l = (long) f;
+	       System.out.println(l);
+		}
+	}
+
+
+
