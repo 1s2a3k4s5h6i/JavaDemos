ea3dae0ad0273c414c5508cd66fb34a6455c210e
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 20:20:28 +0530

    Ternary Operators in Java Programming

---
+++ Java demos/src/com/capgimini/Operators/TerneryOperators.java
@@ -0,0 +1,17 @@
+package com.capgimini.Operators;
+
+public class TerneryOperators {
+
+		public static void main(String[] args) 
+		{
+			int x = 10;
+			
+			x = (10 == x) ? 1 : 0;
+			
+			System.out.println(x);
+	     
+		}
+
+	}
+
+
---
+++ Java demos/src/com/capgimini/Operators/TernoryOperators.java
@@ -0,0 +1,18 @@
+package com.capgimini.Operators;
+
+public class TernoryOperators {
+
+
+		public static void main(String[] args) 
+		{
+			int x = 10;
+			
+			x = (10 == x) ? 1 : 0;
+			
+			System.out.println(x);
+	     
+		}
+
+	}
+
+
