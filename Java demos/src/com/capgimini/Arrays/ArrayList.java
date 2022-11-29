45b9ac865811e127f3e14c337504669d5dbef6b0
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 22:21:41 +0530

    ArrayList in java programming

---
+++ Java demos/src/com/capgimini/Arrays/ArrayList.java
@@ -0,0 +1,39 @@
+package com.capgimini.Arrays;
+
+	import java.util.List;
+
+	public class ArrayList {
+		
+			
+			public static void main(String[] args) {
+//				List<String> obj=new LinkedList<>();
+//				obj.add("India");
+//				obj.add("USA");
+//				obj.add("UK");
+//				obj.add("Korea");
+//				obj.add("Poland");
+//				
+//				System.out.println(obj);
+//				
+				
+		List<String> obj1=(List<String>) new ArrayList();
+				obj1.add("India");
+				obj1.add("USA");
+				obj1.add("UK");
+				obj1.add("Korea");
+				obj1.add("Poland");
+				
+				System.out.println(obj1);
+				
+				
+			
+			}
+		
+				
+				
+
+			
+
+
+	}
+
