64d54082c67af86849e500b19eaa8a2ea28bd8fd
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 20:21:48 +0530

    Assignment Operators in Java Programming

---
+++ Java demos/src/com/capgimini/Operators/AssignmentOperators.java
@@ -0,0 +1,18 @@
+package com.capgimini.Operators;
+
+public class AssignmentOperators {
+	
+		public static void main(String[] args) 
+		{
+			int x = 4;
+			//x += 5; // x = x + 5
+			//x -= 5; // x = x - 5
+			//x *= 5; // x = x * 5
+			//x /= 4; // x = x / 4
+			x %= 5;   // x = x % 5
+			System.out.println(x);
+		}
+	}
+
+
+
