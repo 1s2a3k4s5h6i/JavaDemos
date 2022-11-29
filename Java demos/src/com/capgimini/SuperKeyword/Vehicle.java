a3e03fa952c392171928f2ccbb6e691d4ee6ee5e
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 21:25:36 +0530

    Super keyword in java programming example 1

---
+++ Java demos/src/com/capgimini/SuperKeyword/Vehicle.java
@@ -0,0 +1,23 @@
+package com.capgimini.SuperKeyword;
+
+public class Vehicle {
+	
+	int maxSpeed = 120;
+	}
+	class Car extends Vehicle
+	{
+	int maxSpeed = 180;
+	void display()
+	{
+	System.out.println("Maximum Speed: " +maxSpeed);
+	}
+	}
+	class S
+	{
+	public static void main(String[] args)
+	{
+	Car small = new Car();
+	small.display();
+	}
+	}
+
