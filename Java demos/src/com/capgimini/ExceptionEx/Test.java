7da1ca8c84f5efead1cb347669b94d6d2a50dc5a
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 22:29:43 +0530

    Test demo

---
+++ Java demos/src/com/capgimini/ExceptionEx/Test.java
@@ -0,0 +1,33 @@
+package com.capgimini.ExceptionEx;
+
+
+	public class Test {
+
+		public static void main(String[] args) {
+	  
+	      int arr[]=new int[7];
+	      
+	      try {
+			arr[10]=arr[11];
+			
+//		} catch (ArithmeticException e) {
+//			e.printStackTrace();
+//		}
+//	      catch (ArrayIndexOutOfBoundsException e) {
+//	  		e.printStackTrace();
+	   }
+	      catch (Exception e) {
+	    		e.printStackTrace();
+	        }
+	              
+	               
+	      System.out.println("Print Me");
+	   
+				
+			
+	}
+		
+	}
+
+
+
