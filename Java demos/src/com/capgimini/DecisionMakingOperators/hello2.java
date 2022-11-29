f3301a148dcd7c24d9b5d98244fa838282f1b5d7
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 20:05:50 +0530

    Decision making operators in java programming

--- Java demos/src/com/capgimini/DecisionMakingOperators/hello2.java
+++ Java demos/src/com/capgimini/DecisionMakingOperators/hello2.java
@@ -1,19 +1,20 @@
 package com.capgimini.DecisionMakingOperators;
 
 public class hello2 {
-		public static void main(String[] args) 
-		{
-			int x = 8, y = 7;
+
+		public static void main(String[] args) {
 			
-	           if(x >= y)
-	           {
-	        	   System.out.println("true");
-	           }
-	           else
-	           {
-	        	   System.out.println("false");
-	           }
+			 int x = 10; int y = 11;
+			  
+			  if(!(x < y) || (x == y))
+			  { 
+				  System.out.println("Condition is TRUE");
+			  }
+			  else
+			  { 
+				  System.out.println("Condition is FALSE"); 
+			}
+			 
+
 		}
-	}
-
-
+}
