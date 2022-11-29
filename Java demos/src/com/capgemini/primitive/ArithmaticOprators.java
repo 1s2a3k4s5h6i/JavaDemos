12657da4a74a9ff612e0523336f43faeeddd842d
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 19:53:41 +0530

    Arithmatic Oprators in Java program

---
+++ Java demos/src/com/capgemini/primitive/ArithmaticOprators.java
@@ -0,0 +1,16 @@
+package com.capgemini.primitive;
+
+public class ArithmaticOprators  
+	{
+		public static void main(String[] args) {
+			
+			int x = 10, y=20;
+			System.out.println(x+y);
+			System.out.println(--x);
+			System.out.println(++y);
+
+		}
+
+	}
+
+
