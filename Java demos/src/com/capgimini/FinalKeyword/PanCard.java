314d4c53c8b4265628a660a7ea4f505612aa6fbd
HP <HP@LAPTOP-0008Q3N9>
2022-11-29 21:16:27 +0530

    Final Keyword in java programming

---
+++ Java demos/src/com/capgimini/FinalKeyword/PanCard.java
@@ -0,0 +1,24 @@
+package com.capgimini.FinalKeyword;
+
+public class PanCard {
+	
+	//final int const = 5; // 1-way
+	//static final int a;
+	final int constant; // 2-way
+	/*
+	S()
+	{
+	const = 5;
+	}
+	*/
+	// 3rd way
+	{
+	System.out.println("Here i am");
+	constant = 5;
+	}
+	public static void main(String[] args)
+	{
+	PanCard obj = new PanCard();
+	}
+	}
+
