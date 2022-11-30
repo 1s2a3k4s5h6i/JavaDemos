c142fee798ceff7267ac9ddbfdd26ff32d9aeacb
HP <HP@LAPTOP-0008Q3N9>
2022-11-30 21:18:24 +0530

    Example 2

---
+++ Java demos/src/com/capgimini/javaset/HashSetDe.java
@@ -0,0 +1,27 @@
+package com.capgimini.javaset;
+	import java.util.HashSet;
+	import java.util.LinkedHashSet;
+	import java.util.Set;
+	import java.util.TreeSet;
+
+	public class HashSetDe {
+
+		public static void main(String[] args) {
+			
+				
+			TreeSet<String> num=new TreeSet<String>();
+			
+			for (int i=30;i>0;i--)
+			{
+			num.add("AB"+i);
+			 
+			}
+			for( String i:num)
+			{
+				System.out.println(i);
+			}
+			    
+		}
+
+	}
+
