334947620348dc1aacb72da734244907cc4b304f
HP <HP@LAPTOP-0008Q3N9>
2022-11-30 21:14:06 +0530

    String

---
+++ Java demos/src/com/capgimini/Stringdemo/StringEx.java
@@ -0,0 +1,24 @@
+package com.capgimini.Stringdemo;
+
+public class StringEx {
+
+		public static void main(String[] args) {
+
+			
+			String str=new String("Java Program");
+			
+			String str1=new String("java program");
+			
+			System.out.println(str);
+			
+			System.out.println(str.charAt(3));
+			
+			System.out.println(str.equalsIgnoreCase(str1));
+			
+			String str2=str.substring(5);
+			System.out.println(str2);
+			
+		}
+
+	}
+
